dc7e1be790d0189f897a7746fd4e5e13cdfbc9b8
Ustin <ustinx@mail.ru>
2019-12-05 22:37:02 +0300

    11th lesson - initil page template, works fine

--- firstspa/src/App.js
+++ firstspa/src/App.js
@@ -4,21 +4,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+
+      <header className="header">
+        <img alt='' src='https://i-love-png.com/images/yoga-logo-png-25.png' />
       </header>
+
+      <nav className="nav">
+        <div><a>Profile</a></div>
+        <div><a>Messages</a></div>
+        <div><a>News</a></div>
+        <div><a>Music</a></div>
+        <div><a>Settings</a></div>
+      </nav>
+      
+    <div className="content">
+      <div><img className='content-main-image' src='https://www.canon.ru/media/EOS_5D_Mark_IV_CINC%20_sample-image2_1920x520_hero_tcm203-1374061.jpg'></img></div>
+      <div>
+        ava + post
+        <div><img src='https://s3.amazonaws.com/liberty-uploads/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg'></img></div>
+        
+        </div>
+      <div>My post</div>
+      <div>New post</div>
+      <div>Posts:
+          <div>Post 1</div>
+          <div>Post 2</div>
+          <div>Post 3</div>
+          <div>Post 4</div>
+      </div>
+    </div>
+
     </div>
   );
 }
