66c8e09591ae7b04f3d0bfdaf720bd3f01639f29
Ustin <ustinx@mail.ru>
2019-12-06 20:11:19 +0300

    15th - add folders for components

--- firstspa/src/components/Profile/Profile.jsx
+++ firstspa/src/components/Profile/Profile.jsx
@@ -7,7 +7,7 @@ const Profile = () => {
       <div><img className={css.content_main_image} alt="" src='https://www.canon.ru/media/EOS_5D_Mark_IV_CINC%20_sample-image2_1920x520_hero_tcm203-1374061.jpg'></img></div>
       <div>
         ava + post
-        <div><img src='https://s3.amazonaws.com/liberty-uploads/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg'></img></div>
+        <div><img className={css.avatar} alt='avatar' src='https://s3.amazonaws.com/liberty-uploads/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg'></img></div>
         
         </div>
       <div>My post</div>
