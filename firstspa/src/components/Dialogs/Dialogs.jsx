132ad2422e46f245d294eb95d15523b3529e22bf
Ustin <ustinx@mail.ru>
2019-12-07 19:29:25 +0300

    22th lesson - dialogs temlate

--- firstspa/src/components/Dialogs/Dialogs.jsx
+++ firstspa/src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,29 @@ import css from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={css.dialogs}>
+            <div className={css.dialogsItems}>
+                <div className={css.dialog + ' ' + css.active}>
+                    Dima
+                </div>
+                <div className={css.dialog}>
+                    Sveta
+                </div>
+                <div className={css.dialog}>
+                    Katya
+                </div>
+                <div className={css.dialog}>
+                    Kolya
+                </div>
+                <div className={css.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={css.messages}>
+                <div className={css.message}>Hi!</div>
+                <div className={css.message}>How are you ?</div>
+                <div className={css.message}>Yo!</div>
+            </div>
         </div>
     )
 }
