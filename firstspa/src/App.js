354df1d5e85c6744a3cfea389ed0c5d0be1016a9
Ustin <ustinx@mail.ru>
2019-12-07 23:27:55 +0300

    30th move BrowserRouter to index.js

--- firstspa/src/App.js
+++ firstspa/src/App.js
@@ -4,12 +4,11 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 function App(props) {
   debugger;
   return (
-    <BrowserRouter>
       <div className="app-wrapper">
         <Header />
         <Navbar />
@@ -18,7 +17,6 @@ function App(props) {
           <Route path='/dialogs' render={() => <Dialogs state={props.state.dialogsPage} />} />
         </div>
       </div>
-    </BrowserRouter>
   );
 }
 
