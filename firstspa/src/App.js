19003ab7e005668cf6e986d47975b16ad1107b33
Ustin <ustinx@mail.ru>
2019-12-07 22:38:58 +0300

    28th - render instead of component for passing props

--- firstspa/src/App.js
+++ firstspa/src/App.js
@@ -5,24 +5,19 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-import {Route, BrowserRouter} from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 
-function App() {
+function App(props) {
   return (
     <BrowserRouter>
-    <div className="app-wrapper">
-
-      <Header />
-
-      <Navbar />
-
-      <div className="app-wrapper-content">
-        <Route path='/profile' component={Profile} />
-        <Route path='/dialogs' component={Dialogs} />
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Route path='/profile' render={() => <Profile />} />
+          <Route path='/dialogs' render={() => <Dialogs />} />
+        </div>
       </div>
-
-      {/* <Profile /> */}
-    </div>
     </BrowserRouter>
   );
 }
