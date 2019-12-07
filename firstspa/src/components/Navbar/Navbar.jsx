20bd09abc404652c8ea4169ebe22590100e3a56a
Ustin <ustinx@mail.ru>
2019-12-07 19:06:12 +0300

    21th - https://www.youtube.com/watch?v=Wm62LRtxomk&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=21

--- firstspa/src/components/Navbar/Navbar.jsx
+++ firstspa/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import css from './Navbar.module.css';
+import {NavLink} from 'react-router-dom'; 
 
 const Navbar = () => {
     return(
         <nav className={css.nav}>
-        <div className={css.item}><a href='/profile'>Profile</a></div>
-        <div className={css.item}><a href='/dialogs'>Messages</a></div>
-        <div className={css.item}><a href='profile'>News</a></div>
-        <div className={css.item}><a href='profile'>Music</a></div>
-        <div className={css.item}><a href='profile'>Settings</a></div>
+        <div className={css.item}><NavLink activeClassName={css.active} to='/profile'>Profile</NavLink></div>
+        <div className={css.item}><NavLink activeClassName={css.active} to='/dialogs'>Messages</NavLink></div>
+        <div className={css.item}><NavLink to='profile'>News</NavLink></div>
+        <div className={css.item}><NavLink to='profile'>Music</NavLink></div>
+        <div className={css.item}><NavLink to='profile'>Settings</NavLink></div>
       </nav>
     )
 }
