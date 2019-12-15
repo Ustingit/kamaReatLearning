29821104119faa1ff4c59628742a2e1737803fc4
Ustin <ustinx@mail.ru>
2019-12-15 21:39:03 +0300

    53th

--- firstspa/src/components/Navbar/Navbar.jsx
+++ firstspa/src/components/Navbar/Navbar.jsx
@@ -7,6 +7,7 @@ const Navbar = () => {
         <nav className={css.nav}>
         <div className={css.item}><NavLink activeClassName={css.active} to='/profile'>Profile</NavLink></div>
         <div className={css.item}><NavLink activeClassName={css.active} to='/dialogs'>Messages</NavLink></div>
+        <div className={css.item}><NavLink activeClassName={css.active} to='/users'>Users</NavLink></div>
         <div className={css.item}><NavLink to='profile'>News</NavLink></div>
         <div className={css.item}><NavLink to='profile'>Music</NavLink></div>
         <div className={css.item}><NavLink to='profile'>Settings</NavLink></div>
--- firstspa/src/components/Users/Users.jsx
+++ firstspa/src/components/Users/Users.jsx
@@ -3,48 +3,52 @@ import css from './Users.module.css';
 import Axios from 'axios';
 import image from './../../assets/images/avaDefault2.png';
 
-const Users = (props) => {
-  let getUsers = () => {
-    if(props.users.length === 0){
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getUsers();
+  }
+
+  getUsers = () => {
+    if (this.props.users.length === 0) {
       Axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-        debugger
-        props.setUsers(response.data.items);
-        debugger
+        this.props.setUsers(response.data.items);
       });
     }
   }
 
-  return (
-    <div>
-      <button onClick={getUsers}>Get users</button>
-      {props.users.map(x =>
+  render() {
+    return (
+      <div>
+        { this.props.users.map(x =>
 
-        <div key={x.id}>
-          <span>
-            <div>
-              <img className={css.userPhoto} 
-                   alt='avatar' 
-                   src={ x.photos.small ? x.photos.small : image }></img>
-            </div>
-            <div>
-              {x.followed
-                ? <button onClick={() => { props.unfollow(x.id) }}>Unfollow</button>
-                : <button onClick={() => { props.follow(x.id) }}>Follow</button>}
-            </div>
-          </span>
-          <span>
+          <div key={x.id}>
             <span>
-              <div>{x.name}</div><div>{x.status}</div>
+              <div>
+                <img className={css.userPhoto}
+                  alt='avatar'
+                  src={x.photos.small ? x.photos.small : image}></img>
+              </div>
+              <div>
+                {x.followed
+                  ? <button onClick={() => { this.props.unfollow(x.id) }}>Unfollow</button>
+                  : <button onClick={() => { this.props.follow(x.id) }}>Follow</button>}
+              </div>
             </span>
             <span>
-              <div>x.location.country</div>
-              <div>x.location.city</div>
+              <span>
+                <div>{x.name}</div><div>{x.status}</div>
+              </span>
+              <span>
+                <div>x.location.country</div>
+                <div>x.location.city</div>
+              </span>
             </span>
-          </span>
 
-        </div>)}
-    </div>
-  )
+          </div>)}
+      </div>
+    )
+  }
 }
 
 export default Users;
