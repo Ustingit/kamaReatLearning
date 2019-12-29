238f3b358e7b641ef5e1b1de7b27080dbc6a0171
Ustin <ustinx@mail.ru>
2019-12-29 10:15:23 +0300

    75th - npm install redux-form --save, LOGIN refactoring

--- firstspa/src/components/Login/Login.jsx
+++ firstspa/src/components/Login/Login.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form'
+
+const LoginForm = (props) => {
+    return <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field component={'input'} placeholder={'Login'} name={'login'} />
+            </div>
+            <div>
+                <Field component={'input'} placeholder={'Password'} name={'password'} />
+            </div>
+            <div>
+                <Field component={'input'}  type={'checkbox'} name={'rememberMe'} />remember me
+            </div>
+            <div>
+                <button>
+                    login
+                </button>
+            </div>
+        </form>
+}
+
+const LoginReduxFrom = reduxForm({
+    // a unique name for the form
+    form: 'login'
+  })(LoginForm)
 
 const Login = (props) => {
-    return <h1>LOGIN</h1>
+    const onSubmit = (formData) => {
+        console.log(formData);
+    }
+
+    return <div>
+        <h1>Login</h1>
+        <LoginReduxFrom onSubmit={onSubmit} />
+    </div>
 }
 
 export default Login
