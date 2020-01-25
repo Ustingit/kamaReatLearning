d27b83743fce8280e93374a5c4a61011604c5615
Ustin <ustinx@mail.ru>
2020-01-25 21:44:20 +0300

    84th - move status from class component to hook

--- firstspa/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ firstspa/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import css from './ProfileInfo.module.css';
 import Preloader from '../../common/Preloader/Preloader';
-import ProfileStatus from './ProfileStatus';
+import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 
 const ProfileInfo = (props) => {
     if(!props.profile){
@@ -23,7 +23,7 @@ const ProfileInfo = (props) => {
                     alt='avatar' 
                     src={ props.profile.photos.small }>
                     </img>
-                    <ProfileStatus status={props.status} updateStatus={props.updateStatus} />
+                    <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
                 </div>
             </div>
         </div>
