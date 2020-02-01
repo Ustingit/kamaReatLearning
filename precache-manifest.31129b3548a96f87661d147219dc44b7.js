cf8f997630b5777cf34b1f99eebc55132e27d23f
Ustin <ustinx@mail.ru>
2020-02-01 21:31:17 +0300

    Updates

---
+++ precache-manifest.31129b3548a96f87661d147219dc44b7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9429f8fdf063343a996eb9204fb23b",
+    "url": "/Ustingit/ustinSocNet/index.html"
+  },
+  {
+    "revision": "dd527ee573d99aa2fb14",
+    "url": "/Ustingit/ustinSocNet/static/css/3.693c3a34.chunk.css"
+  },
+  {
+    "revision": "7894644fa98b1e856ffc",
+    "url": "/Ustingit/ustinSocNet/static/css/4.25fd652b.chunk.css"
+  },
+  {
+    "revision": "0e7cfe34b547a910c71c",
+    "url": "/Ustingit/ustinSocNet/static/css/main.9ef0402b.chunk.css"
+  },
+  {
+    "revision": "e10c03207ea634a1d3e0",
+    "url": "/Ustingit/ustinSocNet/static/js/2.239ac76f.chunk.js"
+  },
+  {
+    "revision": "dd527ee573d99aa2fb14",
+    "url": "/Ustingit/ustinSocNet/static/js/3.ad241bdb.chunk.js"
+  },
+  {
+    "revision": "7894644fa98b1e856ffc",
+    "url": "/Ustingit/ustinSocNet/static/js/4.6e8a9682.chunk.js"
+  },
+  {
+    "revision": "0e7cfe34b547a910c71c",
+    "url": "/Ustingit/ustinSocNet/static/js/main.0a1fd6b0.chunk.js"
+  },
+  {
+    "revision": "7c436a81ac52450fe088",
+    "url": "/Ustingit/ustinSocNet/static/js/runtime-main.477fa253.js"
+  },
+  {
+    "revision": "6b0022312d41080436c52da571d5c697",
+    "url": "/Ustingit/ustinSocNet/static/media/avaDefault2.6b002231.png"
+  }
+]);
